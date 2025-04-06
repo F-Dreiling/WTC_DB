af918374af5a89690fb929bb0c6e04da05962b20
FX Drooklyn <dr.f380@hotmail.com>
2025-04-06 18:40:49 +0200

    frontend styling

--- index.php
+++ index.php
@@ -94,59 +94,84 @@
     </script>
     <script src="actions.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
+    
     <title>DB Viewer</title>
 </head>
 <body>
-    <div id="content" class="container mt-4 mb-2">
-        <h1>Welcome to DB Viewer</h1>
+    <div id="content" class="container d-flex flex-column align-items-center text-center mt-4 mb-2">
+        <h1 class="w-75">Welcome to DB Viewer</h1>
 
         <?php if (isset($_SESSION['error'])) : ?>
-            <p class='text-danger'><?= htmlentities($_SESSION['error']); ?></p><br>
+            <p class='text-danger w-75'><?= htmlentities($_SESSION['error']); ?></p>
             <?php unset($_SESSION['error']); ?>
         <?php endif; ?>
 
         <?php if (isset($_SESSION['success'])): ?>
-            <p class='text-success'><?= htmlentities($_SESSION['success']); ?></p>
+            <p class='text-success w-75'><?= htmlentities($_SESSION['success']); ?></p>
             <?php unset($_SESSION['success']); ?>
         <?php endif; ?>
 
         <?php if (isset($_SESSION['load'])): ?>
 
-        <div>
+        <div class="w-75">
             <?= $response; ?>
         </div>
 
-        <div class="d-flex flex-row align-items-center gap-2">
-            <form action="index.php" method="POST" class="me-2">
+        <div class="d-flex justify-content-between align-items-center gap-2 w-75">
+            <a href='reset.php' type='button' class='btn btn-secondary'>&lt;&lt; Reset</a>
+
+            <form action="index.php" method="POST">
                 <label for="table">Table:</label>
-                <input type="text" name="table" id="table" value="<?= htmlentities($_SESSION['table']); ?>">
+                <input type="text" name="table" id="table" value="<?= htmlentities($_SESSION['table']); ?>" style="width: 33%">
                 <input type="submit" class="btn btn-secondary" name="refresh" value="Refresh">
             </form>
 
-            <a href='reset.php' type='button' class='btn btn-secondary me-2'>Reset and Back</a>
-            <input onclick='printJson()' type='button' class='btn btn-secondary' value='Print JSON to Log'>
+            <input onclick='printJson()' type='button' class='btn btn-secondary' value='Print JSON'>
         </div>
 
         <?php else: ?>
 
-        <p class="text-info">Please enter the database and table name, username and optionally password.</p><br>
-        <p>
-            <form action="index.php" method="POST">
-                <label for="host">Host:</label>
-                <input type="text" name="host" id="host" value="localhost"><br>
-                <label for="port">Port:</label>
-                <input type="text" name="port" id="port" value="3306"><br><br>
-                <label for="name">DB Name:</label>
-                <input type="text" name="dbName" id="dbName" placeholder="Enter the database name"><br>
-                <label for="userName">User Name:</label>
-                <input type="text" name="userName" id="userName" placeholder="Enter your username"><br>
-                <label for="passWord">Password:</label>
-                <input type="text" name="passWord" id="passWord" placeholder="Enter your password"><br><br>
-                <label for="table">Table:</label>
-                <input type="text" name="table" id="table" placeholder="Enter the table to display"><br><br>
-                <input type="submit" class="btn btn-primary" name="submit" value="Submit">
+        <p class="text-warning w-75">Please enter the database and table name, username and optionally password.</p>
+
+        <div class="w-75">
+            <form action="index.php" method="POST" class="d-flex flex-column gap-3">
+                <div class="row">
+                    <div class="col-md-6">
+                        <label for="host">Host:</label>
+                        <input type="text" name="host" id="host" class="form-control" value="localhost">
+                    </div>
+                    <div class="col-md-6">
+                        <label for="port">Port:</label>
+                        <input type="text" name="port" id="port" class="form-control" value="3306">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-6">
+                        <label for="userName">User Name:</label>
+                        <input type="text" name="userName" id="userName" class="form-control" placeholder="Enter your username">
+                    </div>
+                    <div class="col-md-6">
+                        <label for="passWord">Password:</label>
+                        <input type="text" name="passWord" id="passWord" class="form-control" placeholder="Enter your password">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-6">
+                        <label for="dbName">DB Name:</label>
+                        <input type="text" name="dbName" id="dbName" class="form-control" placeholder="Enter the database">
+                    </div>
+                    <div class="col-md-6">
+                        <label for="table">Table:</label>
+                        <input type="text" name="table" id="table" class="form-control" placeholder="Enter the table">
+                    </div>
+                </div>
+                <div class="row mt-3">
+                    <div class="col">
+                        <input type="submit" class="btn btn-secondary w-100" name="submit" value="Submit">
+                    </div>
+                </div>
             </form>
-        </p>
+        </div>
 
         <?php endif; ?>
     </div>
